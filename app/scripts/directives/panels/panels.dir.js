23b62491a39bdf738aedf598624d7970c7d28ff1
Oshri Kdoshim <oshrikdoshim@Oshris-MacBook-Pro.local>
2015-03-24 18:46:08 +0200

    Add animation to panels

---
+++ app/scripts/directives/panels/panels.dir.js
@@ -0,0 +1,69 @@
+(function(angular,$){
+    'use-strict';
+
+    angular.module('panelsApp').directive('panels', Panels);
+
+    function Panels () {
+        var factory = {};
+
+        factory.strict = 'A';
+        factory.scope = true;
+        factory.link = function (scope, el, attrs){
+            var activePanel,
+                activePanelType,
+                activePanelSize;
+
+            scope.mask = false; 
+        
+            scope.open = function(panel, type, size, options){
+                if (type == 'push') {
+                    $(el).addClass('push-' + panel).addClass(size);
+                    activePanel = 'push-' + panel;
+                    activePanelType = type;
+                    activePanelSize = size;
+                    activeMask();
+                } else {
+                   $(el).find('.' + panel).addClass('open');
+                   activePanel =  panel;
+                   activePanelType = type;
+                   activePanelSize = size;
+                   activeMask();
+                }
+            };
+
+            scope.addModal = function(){
+
+            };
+
+            scope.close = function(){
+                disabledMask();
+                closeActivePanel();
+            };
+
+            function closeActivePanel () {
+                if (activePanelType == 'push') {
+                    $(el).removeClass('push-' + activePanel).removeClass(activePanelSize);
+                } else {
+                    $(el).find('.' + activePanel).removeClass('open');
+                }
+            }
+
+            function activeMask (){
+                $(el).find('.panel-mask').addClass('show')
+                scope.mask = true;
+            }
+
+            function disabledMask () {
+                $(el).find('.panel-mask').removeClass('show')
+                scope.mask = false;
+            }
+
+        };
+
+        return factory;
+    }
+
+    Panels.$inject = [];
+
+
+})(window.angular,window.jQuery);
